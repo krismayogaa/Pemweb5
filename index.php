cf02f8b143dfbf5f48c2dd0266c2235788da7196
krismayogaa <tag.krismayogaa@gmail.com>
2019-09-30 16:08:07 +0700

    berubah cong

--- index.php
+++ index.php
@@ -5,8 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible:" content="ie-edge"/>
     <title>Insert Data Mahasiswa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   </head>
   <body>
-    
+    <div class="row justify-content-center">
+    <form action="index.html" method="post">
+      <div class="form-group">
+        <label>Nama</label>
+        <input type="text" name="name" class="form-control" value="Masukkan Nama">
+      </div>
+      <div class="form-group">
+        <label>Alamat</label>
+        <input type="text" name="alamat" class="form-control" value="Masukkan Alamat">
+      </div>
+      <div class="form-group">
+        <label Submit></label>
+        <button type="submit" class="btn btn-primary" name="save">Save</button>
+      </div>
+    </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
   </body>
 </html>
